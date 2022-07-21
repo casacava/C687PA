c732ae39b045725dfa54f65e30ae6db1ff4dfb66
cassandra.cavazos <cassandra.cavazos@coinmetrics.io>
2022-07-20 20:27:31 -0500

    added roster.h data

--- C687PA/roster.h
+++ C687PA/roster.h
@@ -5,9 +5,26 @@
 //  Created by Cass on 7/19/22.
 //
 #include <string>
+#include <array>
 #include “student.h”
 #ifndef roster.h
 #define roster.h
 
+class Roster {
+protected:
+    Student *classRosterArray[5];
+    
+public:
+    void add(string studentID, string firstName, string lastName, string emailaddress, int age, int num1, int num2, int num3, Degree degree);
+    void remove(string studentID);
+    void printAll();
+    void printAverageDaysInCourse(string studentID);
+    void printInvalidEmails();
+    void printByDegreeProgram(Degree degree);
+    
+    int iterator = 0;
+    
+    ~Roster();
+};
 
 #endif /* roster_h */
